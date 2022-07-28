9d9307dd8da276f7eb139c6e026abfacf18ad38e
pablolucio97 <pablolucio_@hotmail.com>
2022-07-28 00:25:01 -0300

    style: basic player page created

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
+import { PrimaryButton } from '../components/Elements/PrimaryButton';
+import { YoutubePlayer } from '../components/Elements/YoutubePlayer';
+import { SubTitle } from '../components/Typography/SubTitle';
+import { Text } from '../components/Typography/Text';
+import { Title } from '../components/Typography/Title';
 import {
     Container,
     VideoContainer,
     IconsContainer,
-    DownloadsContainer
+    DownloadsContainer,
+    BsClockIcon,
+    BsHeartIcon,
+    BsShareIcon
 } from '../styles/player';
 
 const Player = () => {
     return (
         <Container>
             <VideoContainer>
-                <iframe
-                    src="https://www.youtube.com/embed/TXFOSt9uli4"
-                 
+                <YoutubePlayer
+                    videoId='G0x6DJ9eVsM'
+                />
+                <Title
+                    content='Video1'
+                />
+                <SubTitle
+                    content='Canal1'
+                />
+                <Text
+                    content='35345 views - 1 year ago'
+                />
+                <Text
+                    content='343 likes'
                 />
             </VideoContainer>
             <IconsContainer>
-
+                <BsHeartIcon />
+                <BsClockIcon />
+                <BsShareIcon />
             </IconsContainer>
             <DownloadsContainer>
-
+                <PrimaryButton
+                    ariaLabel='Download video'
+                    onClick={() => { }}
+                    title='Download video'
+                />
+                <PrimaryButton
+                    ariaLabel='Download audio'
+                    onClick={() => { }}
+                    title='Download audio'
+                />
             </DownloadsContainer>
         </Container>
     )
--- src/styles/player.ts
+++ src/styles/player.ts
@@ -1,26 +1,64 @@
 import styled from 'styled-components'
+import { BsHeart, BsShare, BsClock } from 'react-icons/bs'
 
 export const Container = styled.div`
 display: flex;
 justify-content: center;
-align-items: center;
-margin: ${({ theme }) => theme.sizes.huge} auto;
+flex-direction: column;
+margin: -${({ theme }) => theme.sizes.huge} auto 0;
+min-height: 100vh;
 `
 
 export const VideoContainer = styled.div`
 display: flex;
+flex-direction: column;
+& h1{
+    margin-top: ${({ theme }) => theme.sizes.xxsmall};
+}
+& h3{
+    margin-bottom: ${({ theme }) => theme.sizes.xxsmall};
+}
+& p:last-child{
+    margin-top:  0;
+}
+margin: 0 auto;
 `
 
 export const IconsContainer = styled.div`
 display: flex;
-flex-direction: column;
 width: 320px;
-background-color: red;
+margin: ${({ theme }) => theme.sizes.small} 0;
 `
 
 export const DownloadsContainer = styled.div`
 display: flex;
 flex-direction: column;
 width: 320px;
-background-color: orange;
+
+& button{
+margin-top: ${({ theme }) => theme.sizes.xsmall};
+color: ${(props) => props.theme.colors.white100};
+}
+`
+
+export const BsHeartIcon = styled(BsHeart)`
+margin-top: ${({ theme }) => theme.sizes.xsmall};
+margin-right: ${({ theme }) => theme.sizes.small};
+width: 20px;
+height: 20px;
+cursor: pointer;
+`
+export const BsClockIcon = styled(BsClock)`
+margin-top: ${({ theme }) => theme.sizes.xsmall};
+margin-right: ${({ theme }) => theme.sizes.small};
+width: 20px;
+height: 20px;
+cursor: pointer;
+`
+export const BsShareIcon = styled(BsShare)`
+margin-top: ${({ theme }) => theme.sizes.xsmall};
+margin-right: ${({ theme }) => theme.sizes.small};
+width: 20px;
+height: 20px;
+cursor: pointer;
 `
